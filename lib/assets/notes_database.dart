cfda811ecb7be1c7f59f98aca90fd76619fd1104
Dinath01 <dinithnethmina101@gmail.com>
2024-05-04 10:39:05 +0530

    Isar Package intergrated followed with the CRUD operation implementation for the application database

---
+++ lib/assets/notes_database.dart
@@ -0,0 +1,45 @@
+import 'package:isar/isar.dart';
+import 'package:notes/assets/notes.dart';
+import 'package:path_provider/path_provider.dart';
+
+class NotesDB{
+  static late Isar isar;
+
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(
+      [NotesSchema],
+      directory: dir.path,
+    );
+  }
+
+  final List<Notes> currentNotes = [];
+
+  Future<void> createNote(String textFromUser) async {
+    final newNote = Notes()..text = textFromUser;
+
+    await isar.writeTxn(() => isar.notes.put(newNote));
+
+    fetchNotes();
+  }
+
+  Future<void> fetchNotes() async {
+    List<Notes> fetchedNotes = await isar.notes.where().findAll();
+    currentNotes.clear();
+    currentNotes.addAll(fetchedNotes);
+  }
+
+  Future<void> updateNote (int id, String newText) async{
+    final existingNote = await isar.notes.get(id);
+    if (existingNote != null){
+      existingNote.text= newText;
+      await isar.writeTxn(() => isar.notes.put(existingNote));
+      fetchNotes();
+    }
+  }
+
+  Future<void> deleteNote(int id) async {
+    await isar.writeTxn(() => isar.notes.delete(id));
+    fetchNotes();
+  }
+}
