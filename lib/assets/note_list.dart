07c2c49aefb34d404d435153c54d15df3bc4dc3d
Dinath01 <dinithnethmina101@gmail.com>
2024-05-04 15:44:15 +0530

    More UI Adjustments

--- lib/assets/note_list.dart
+++ lib/assets/note_list.dart
@@ -17,17 +17,28 @@ class NoteList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.primary,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
-        child: ListTile(
-          title: Text(text),
-          trailing: IconButton(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.only(top: 10, left: 25, right: 25),
+      child: ListTile(
+        title: Text(text),
+        trailing: Builder(
+          builder: (context) => IconButton(
             icon: const Icon(Icons.more_vert),
-            onPressed: () => showPopover(context: context, bodyBuilder: (context) => noteSettings()),
-          )
-        ));
+            onPressed: () => showPopover(
+                width: 100,
+                height: 100,
+                backgroundColor: Theme.of(context).colorScheme.background,
+                context: context,
+                bodyBuilder: (context) => noteSettings(
+                  onEditTap: onEditPressed,
+                  onDeleteTap: onDeletePressed,
+                )),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/assets/tile_settings.dart
+++ lib/assets/tile_settings.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 class noteSettings extends StatelessWidget {
-  const noteSettings({super.key});
+  final void Function()? onEditTap;
+  final void Function()? onDeleteTap;
+
+  const noteSettings({
+    super.key,
+    required this.onEditTap,
+    required this.onDeleteTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onEditTap,
+          child: Container(
+            height: 50,
+            color: Theme.of(context).colorScheme.background,
+            child: Center(
+                child: Text(
+              'Edit',
+              style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  fontWeight: FontWeight.bold),
+            )),
+          ),
+        ),
+        GestureDetector(
+          onTap: onDeleteTap!,
+          child: Container(
+            height: 50,
+            color: Theme.of(context).colorScheme.background,
+            child: Center(
+                child: Text('Delete',
+                    style: TextStyle(
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontWeight: FontWeight.bold))),
+          ),
+        ),
+      ],
     );
   }
 }
